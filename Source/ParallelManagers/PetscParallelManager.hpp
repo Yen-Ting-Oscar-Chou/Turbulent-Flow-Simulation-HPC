e02cdeecc1f34b6793571dc0eb59aec8f5f86d4a
Alexander Stephan <alexander.stephan@tum.de>
2024-11-17 17:39:25 +0100

    Start with PetscParallelManager implementation

---
+++ Source/ParallelManagers/PetscParallelManager.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "FlowField.hpp"
+#include "Parameters.hpp"
+
+#include "../stencils/PressureBufferFillStencil.hpp"
+#include "../stencils/PressureBufferReadStencil.hpp"
+
+#include "../stencils/VelocityBufferFillStencil.hpp"
+#include "../stencils/VelocityBufferReadStencil.hpp"
+
+class PetscParallelManager {
+
+  public:
+    FlowField flowfield_;
+    const Parameters& _parameters;
+
+    struct BufferSize {
+      int LR; // Left-Right
+      int TB; // Top-Bottom
+      int FB; // Front-Back
+    };
+
+    BufferSize presBufSize; // Pressure buffer sizes
+    BufferSize velBufSize;  // Velocity buffer sizes
+
+    // TODO: Can we use vectors?
+    struct Buffers {
+      RealType* leftSend;
+      RealType* rightSend;
+      RealType* bottomSend;
+      RealType* topSend;
+      RealType* frontSend;
+      RealType* backSend;
+
+      RealType* leftRecv;
+      RealType* rightRecv;
+      RealType* bottomRecv;
+      RealType* topRecv;
+      RealType* frontRecv;
+      RealType* backRecv;
+    };
+
+    Buffers pressureBuffers;
+    Buffers velocityBuffers;
+
+    Stencils::PressureBufferFillStencil* _pressureBufferFillStencil;
+    Stencils::PressureBufferReadStencil* _pressureBufferReadStencil;
+
+    ParallelBoundaryIterator<FlowField>* _parallelBoundaryPressureFillIterator;
+    ParallelBoundaryIterator<FlowField>* _parallelBoundaryPressureReadIterator;
+    ParallelBoundaryIterator<FlowField>* _parallelBoundaryVelocityFillIterator;
+    ParallelBoundaryIterator<FlowField>* _parallelBoundaryVelocityReadIterator;
+
+    PetscParallelManager(const Parameters & parameters, FlowField & flowField);
+    ~PetscParallelManager();
+
+    void communicatePressure();
+    void communicateVelocities();
+};

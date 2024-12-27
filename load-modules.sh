e4ed456f28845733e3971530e686d47348c96861
Florian Wunderlich <florian.wunderlich@tum.de>
2024-12-27 14:06:50 +0000

    can now compile for both clang and nvhpc

--- load-modules.sh
+++ load-modules.sh
@@ -1,6 +1,23 @@
 #!/bin/bash 
-module load llvm-14.0.0
-module load cuda-11.4.4
-module load cmake-3.23.0 
-module load petsc-3.17.1 
+
+echo "Unloading modules"
+module unload llvm-14.0.0
+module unload nvhpc-23.9
+module unload cuda-11.4.4
+
+# Check for a command-line argument
+if [ "$1" == "llvm" ]; then
+    echo "Loading clang modules"
+    module load llvm-14.0.0
+    module load cuda-11.4.4
+elif [ "$1" == "nvhpc" ]; then
+    echo "Loading nvhpc modules"
+    module load nvhpc-23.9
+else
+    echo "Usage: $0 [llvm|nvhpc]"
+    exit 1
+fi
+
+module load cmake-3.23.0
+module load petsc-3.17.1
 module load openmpi-4.0.2

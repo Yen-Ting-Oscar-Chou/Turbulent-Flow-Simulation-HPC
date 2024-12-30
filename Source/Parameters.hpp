ceec2199acc0740847f5810bd2a4cd6c8661dc83
Florian Wunderlich <florian.wunderlich@tum.de>
2024-12-30 17:08:54 +0000

    parameters changed for new deltaMixLen and updated pragma. Meshsize can still not be mapped

--- Source/Parameters.hpp
+++ Source/Parameters.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "BoundaryType.hpp"
+#include "MixingLengths.hpp"
 #include "Definitions.hpp"
 #include "Meshsize.hpp"
 
@@ -11,8 +12,6 @@ public:
   RealType dt  = 0; //! Timestep
   RealType tau = 0; //! Security factor
 };
-#pragma omp declare mapper(TimestepParameters p)  \
- map(to: p, p.dt, p.tau)
 
 class SimulationParameters {
 public:
@@ -21,8 +20,6 @@ public:
   std::string scenario;      //! If channel or cavity, for example
   std::string velocityProfile = "uniform"; //! block or parabolic, block by default
 };
-#pragma omp declare mapper(SimulationParameters p)  \
- map(to: p, p.finalTime, p.type, p.scenario, p.velocityProfile)
 
 class EnvironmentalParameters {
 public:
@@ -31,30 +28,22 @@ public:
   RealType gy = 0;
   RealType gz = 0;
 };
-#pragma omp declare mapper(EnvironmentalParameters p)  \
- map(to: p, p.gx, p.gy, p.gz)
 
 class FlowParameters {
 public:
   RealType Re = 0; //! Reynolds number
 };
-#pragma omp declare mapper(FlowParameters p)  \
- map(to: p, p.Re)
 
 class TurbulenceParameters {
   public: 
-    std::function<RealType (const RealType, const RealType)> deltaMixLen = NULL;
+    TurbulenceType deltaMixLen = ZERO;
 };
-#pragma omp declare mapper(TurbulenceParameters p)  \
- map(to: p, p.deltaMixLen)
 
 class SolverParameters {
 public:
   RealType gamma         = 0;  //! Donor cell balance coefficient
   int      maxIterations = -1; //! Maximum number of iterations in the linear solver
 };
-#pragma omp declare mapper(SolverParameters p)  \
- map(to: p, p.gamma, p.maxIterations)
 
 class GeometricParameters {
 public:
@@ -79,8 +68,6 @@ public:
   int stretchY = -1;
   int stretchZ = -1;
 };
-#pragma omp declare mapper(GeometricParameters p)  \
- map(to: p, p.dim, p.sizeX, p.sizeY, p.sizeZ,p.lengthX,p.lengthY,p.lengthZ,p.meshsizeType,p.stretchX ,p.stretchY,p.stretchZ)
 
 class WallParameters {
 public:
@@ -108,8 +95,6 @@ public:
   BoundaryType typeFront;
   BoundaryType typeBack;
 };
-#pragma omp declare mapper(WallParameters p)  \
- map(to: p, p.scalarLeft, p.scalarRight, p.scalarBottom, p.scalarTop, p.scalarFront, p.scalarBack, p.typeLeft, p.typeRight, p.typeTop, p.typeBottom, p.typeFront, p.typeBack, p.vectorLeft[0:3], p.vectorRight[0:3], p.vectorBottom[0:3], p.vectorTop[0:3], p.vectorFront[0:3], p.vectorBack[0:3])
 
 
 class VTKParameters {
@@ -117,15 +102,11 @@ public:
   RealType    interval = 0; //! Time interval for file printing
   std::string prefix;       //! Output filename
 };
-#pragma omp declare mapper(VTKParameters v) \
-  map(to: v, v.interval, v.prefix)
 
 class StdOutParameters {
 public:
   RealType interval = 0;
 };
-#pragma omp declare mapper(StdOutParameters s) \
-  map(to: s, s.interval)
 
 class ParallelParameters {
 public:
@@ -153,17 +134,12 @@ public:
   int* sizes[3];
 #endif
 };
-// TODO map sizes[3]
-#pragma omp declare mapper(ParallelParameters p) \
-  map(to: p, p.rank, p.numProcessors[0:3], p.leftNb, p.rightNb, p.bottomNb, p.topNb, p.frontNb, p.backNb, p.indices[0:3], p.localSize[0:3], p.firstCorner[0:3])
 
 class BFStepParameters {
 public:
   RealType xRatio = 0;
   RealType yRatio = 0;
 };
-#pragma omp declare mapper(BFStepParameters b) \
-  map(to: b, b.xRatio, b.yRatio)
 
 //@}
 
@@ -189,6 +165,11 @@ public:
   Meshsize* meshsize;
 };
 #pragma omp declare mapper(Parameters p) \
-  map(to: p.simulation, p.timestep, p.environment, p.flow, p.solver, \
-          p.geometry, p.walls, p.vtk, p.parallel, p.stdOut, \
-          p.bfStep, p.turbulence, p.meshsize) 
+  map(to: p, p.meshsize[0:1]) \
+  map(to: p.timestep) \
+  map(to: p.environment) \
+  map(to: p.flow) \
+  map(to: p.solver) \
+  map(to: p.geometry) \
+  map(to: p.walls, p.walls.vectorLeft[0:3], p.walls.vectorRight[0:3], p.walls.vectorBottom[0:3], p.walls.vectorTop[0:3], p.walls.vectorFront[0:3], p.walls.vectorBack[0:3]) \
+  map(to: p.turbulence)

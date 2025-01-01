0af9b2f5d25760a29a527befe8470b7b99cdcdb1
Florian Wunderlich <florian.wunderlich@tum.de>
2025-01-01 16:54:40 +0000

    improved calling behavior of delegate

--- Source/MeshsizeDelegate.hpp
+++ Source/MeshsizeDelegate.hpp
@@ -6,33 +6,35 @@ class MeshsizeDelegate: public Meshsize {
 private:
   UniformMeshsize    uniform;
   TanhMeshStretching tanh;
+  const MeshsizeType type;
 
 public:
-  const MeshsizeType type;
   MeshsizeDelegate(const GeometricParameters& geometricParameters, const ParallelParameters& parallelParameters);
 
   ~MeshsizeDelegate() = default;
 
-  inline virtual RealType getDx(int i, int j) const override { return (type == Uniform) * uniform.getDx(i, j) + (type == TanhStretching) * tanh.getDx(i, j); }
+  inline virtual RealType getDx(int i, int j) const override { return (type == Uniform) ? uniform.getDx(i, j) : tanh.getDx(i, j); }
 
-  inline virtual RealType getDy(int i, int j) const override { return (type == Uniform) * uniform.getDy(i, j) + (type == TanhStretching) * tanh.getDy(i, j); }
+  inline virtual RealType getDy(int i, int j) const override { return (type == Uniform) ? uniform.getDy(i, j) : tanh.getDy(i, j); }
 
   inline virtual RealType getDx(int i, int j, [[maybe_unused]] int k) const override { return getDx(i, j); }
 
   inline virtual RealType getDy(int i, int j, [[maybe_unused]] int k) const override { return getDy(i, j); }
 
-  inline virtual RealType getDz(int i, int j, int k) const override { return (type == Uniform) * uniform.getDz(i, j, k) + (type == TanhStretching) * tanh.getDz(i, j, k); }
+  inline virtual RealType getDz(int i, int j, int k) const override { return (type == Uniform) ? uniform.getDz(i, j, k) : tanh.getDz(i, j, k); }
 
-  inline virtual RealType getPosX(int i, int j, int k) const override { return (type == Uniform) * uniform.getPosX(i, j, k) + (type == TanhStretching) * tanh.getPosX(i, j, k); }
+  inline virtual RealType getPosX(int i, int j, int k) const override { return (type == Uniform) ? uniform.getPosX(i, j, k) : tanh.getPosX(i, j, k); }
 
-  inline virtual RealType getPosY(int i, int j, int k) const override { return (type == Uniform) * uniform.getPosY(i, j, k) + (type == TanhStretching) * tanh.getPosY(i, j, k); }
+  inline virtual RealType getPosY(int i, int j, int k) const override { return (type == Uniform) ? uniform.getPosY(i, j, k) : tanh.getPosY(i, j, k); }
 
-  inline virtual RealType getPosZ(int i, int j, int k) const override { return (type == Uniform) * uniform.getPosZ(i, j, k) + (type == TanhStretching) * tanh.getPosZ(i, j, k); }
+  inline virtual RealType getPosZ(int i, int j, int k) const override { return (type == Uniform) ? uniform.getPosZ(i, j, k) : tanh.getPosZ(i, j, k); }
 
   inline virtual RealType getPosX(int i, int j) const override { return getPosX(i, j, 0); }
   inline virtual RealType getPosY(int i, int j) const override { return getPosY(i, j, 0); }
 
-  inline virtual RealType getDxMin() const override { return (type == Uniform) * uniform.getDxMin() + (type == TanhStretching) * tanh.getDxMin(); }
-  inline virtual RealType getDyMin() const override { return (type == Uniform) * uniform.getDyMin() + (type == TanhStretching) * tanh.getDyMin(); }
-  inline virtual RealType getDzMin() const override { return (type == Uniform) * uniform.getDzMin() + (type == TanhStretching) * tanh.getDzMin(); }
+  inline virtual RealType getDxMin() const override { return (type == Uniform) ? uniform.getDxMin() : tanh.getDxMin(); }
+  inline virtual RealType getDyMin() const override { return (type == Uniform) ? uniform.getDyMin() : tanh.getDyMin(); }
+  inline virtual RealType getDzMin() const override { return (type == Uniform) ? uniform.getDzMin() : tanh.getDzMin(); }
+
+  inline MeshsizeType getType() const { return type; }
 };

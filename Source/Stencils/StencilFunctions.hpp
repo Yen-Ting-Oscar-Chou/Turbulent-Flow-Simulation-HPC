96ab673886e26c0619ea0867c174ded5cae932ee
Florian Wunderlich <florian.wunderlich@tum.de>
2025-01-08 17:28:25 +0000

    added loadLocal functions for FGH

--- Source/Stencils/StencilFunctions.hpp
+++ Source/Stencils/StencilFunctions.hpp
@@ -40,6 +40,15 @@ namespace Stencils {
     }
   }
 
+  inline void loadLocalVelocity2D(VectorField& velocity, RealType* const localVelocity, int i, int j) {
+    for (int row = -1; row <= 1; row++) {
+      for (int column = -1; column <= 1; column++) {
+        localVelocity[39 + 9 * row + 3 * column]     = velocity.getVectorElement(i + column, j + row, 0); // x-component
+        localVelocity[39 + 9 * row + 3 * column + 1] = velocity.getVectorElement(i + column, j + row, 1); // y-component
+      }
+    }
+  }
+
   // Load the local velocity cube with surrounding velocities
   inline void loadLocalVelocity3D(FlowField& flowField, RealType* const localVelocity, int i, int j, int k) {
     for (int layer = -1; layer <= 1; layer++) {
@@ -49,6 +58,18 @@ namespace Stencils {
           localVelocity[39 + 27 * layer + 9 * row + 3 * column]     = point[0]; // x-component
           localVelocity[39 + 27 * layer + 9 * row + 3 * column + 1] = point[1]; // y-component
           localVelocity[39 + 27 * layer + 9 * row + 3 * column + 2] = point[2]; // z-component
+        }
+      }
+    }
+  }
+
+    inline void loadLocalVelocity3D(VectorField& velocity, RealType* const localVelocity, int i, int j, int k) {
+    for (int layer = -1; layer <= 1; layer++) {
+      for (int row = -1; row <= 1; row++) {
+        for (int column = -1; column <= 1; column++) {
+          localVelocity[39 + 27 * layer + 9 * row + 3 * column]     = velocity.getVectorElement(i + column, j + row, 0); // x-component
+          localVelocity[39 + 27 * layer + 9 * row + 3 * column + 1] = velocity.getVectorElement(i + column, j + row, 1); // y-component
+          localVelocity[39 + 27 * layer + 9 * row + 3 * column + 2] = velocity.getVectorElement(i + column, j + row, 2); // z-component
         }
       }
     }

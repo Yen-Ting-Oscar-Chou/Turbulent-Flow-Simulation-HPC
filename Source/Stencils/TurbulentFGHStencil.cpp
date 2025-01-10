2c0e9d9a377406453925975d9e1053e3d80f9e8f
Florian Wunderlich <florian.wunderlich@tum.de>
2025-01-10 14:47:53 +0000

    added turbulent FGHStencil

--- Source/Stencils/TurbulentFGHStencil.cpp
+++ Source/Stencils/TurbulentFGHStencil.cpp
@@ -18,6 +18,17 @@ void Stencils::TurbulentFGHStencil::apply(const Parameters& parameters, Turbulen
   values[1] = computeTurbulentG2D(localVelocity_, localViscosity_, localMeshsize_, parameters, parameters.timestep.dt);
 }
 
+void Stencils::TurbulentFGHStencil::applyGPU(const Parameters& parameters, VectorField& fgh, ScalarField& viscosity, VectorField& velocity, IntScalarField& flags, int i, int j) {
+  // Load local velocities into the center layer of the local array
+  loadLocalVelocity2D(velocity, localVelocity_, i, j);
+  loadLocalMeshsize2D(parameters, localMeshsize_, i, j);
+  loadLocalViscosity2D(parameters, viscosity, localViscosity_, i, j);
+
+  // Now the localVelocity array should contain lexicographically ordered elements around the given index
+  fgh.getVectorElement(i, j, 0) = computeTurbulentF2D(localVelocity_, localViscosity_, localMeshsize_, parameters, parameters.timestep.dt);
+  fgh.getVectorElement(i, j, 1) = computeTurbulentG2D(localVelocity_, localViscosity_, localMeshsize_, parameters, parameters.timestep.dt);
+}
+
 void Stencils::TurbulentFGHStencil::apply(const Parameters& parameters, TurbulentFlowField& flowField, int i, int j, int k) {
   // The same as in 2D, with slight modifications.
 
@@ -37,6 +48,26 @@ void Stencils::TurbulentFGHStencil::apply(const Parameters& parameters, Turbulen
     }
     if ((obstacle & OBSTACLE_BACK) == 0) {
       values[2] = computeTurbulentH3D(localVelocity_, localViscosity_, localMeshsize_, parameters, parameters.timestep.dt);
+    }
+  }
+}
+
+void Stencils::TurbulentFGHStencil::applyGPU(const Parameters& parameters, VectorField& fgh, ScalarField& viscosity, VectorField& velocity, IntScalarField& flags, int i, int j, int k) {
+  const int       obstacle = flags.getValue(i, j, k);
+
+  if ((obstacle & OBSTACLE_SELF) == 0) { // If the cell is fluid
+    loadLocalVelocity3D(velocity, localVelocity_, i, j, k);
+    loadLocalMeshsize3D(parameters, localMeshsize_, i, j, k);
+    loadLocalViscosity3D(parameters, viscosity, localViscosity_, i, j, k);
+
+    if ((obstacle & OBSTACLE_RIGHT) == 0) { // If the right cell is fluid
+      fgh.getVectorElement(i, j, k, 0) = computeTurbulentF3D(localVelocity_, localViscosity_, localMeshsize_, parameters, parameters.timestep.dt);
+    }
+    if ((obstacle & OBSTACLE_TOP) == 0) {
+      fgh.getVectorElement(i, j, k, 1) = computeTurbulentG3D(localVelocity_, localViscosity_, localMeshsize_, parameters, parameters.timestep.dt);
+    }
+    if ((obstacle & OBSTACLE_BACK) == 0) {
+      fgh.getVectorElement(i, j, k, 2) = computeTurbulentH3D(localVelocity_, localViscosity_, localMeshsize_, parameters, parameters.timestep.dt);
     }
   }
 }
--- Source/Stencils/TurbulentFGHStencil.hpp
+++ Source/Stencils/TurbulentFGHStencil.hpp
@@ -7,20 +7,21 @@
 namespace Stencils {
 
   class TurbulentFGHStencil: public FieldStencil<TurbulentFlowField> {
-  private:
+
+  public:
     // A local velocity variable that will be used to approximate derivatives. Size matches 3D
     // case, but can be used for 2D as well.
     RealType localVelocity_[27 * 3];
     RealType localMeshsize_[27 * 3];
     RealType localViscosity_[27 * 3];
-
-  public:
     TurbulentFGHStencil() = default;
     ~TurbulentFGHStencil() override = default;
 
 #pragma omp declare target
     void apply(const Parameters& parameters, TurbulentFlowField& flowField, int i, int j) override;
     void apply(const Parameters& parameters, TurbulentFlowField& flowField, int i, int j, int k) override;
+    void applyGPU(const Parameters& parameters, VectorField& fgh, ScalarField& viscosity, VectorField& velocity, [[maybe_unused]] IntScalarField& flags, int i, int j);
+    void applyGPU(const Parameters& parameters, VectorField& fgh, ScalarField& viscosity, VectorField& velocity, [[maybe_unused]] IntScalarField& flags, int i, int j, int k);
 #pragma omp end declare target
   };
 
